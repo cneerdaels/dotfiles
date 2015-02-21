ab59d65775d41ea4cdaf181fdf45db9d9b01c7f3
Mike Brown <mikecb@users.noreply.github.com>
2015-02-21 09:21:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#Install some things
+sudo apt-get install git mercurial 
 
+Install some dotfiles.
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # bash
-ln -s ${BASEDIR}/bashrc ~/.bashrc
-ln -s ${BASEDIR}/profile ~/.profile
+ln -fs ${BASEDIR}/bashrc ~/.bashrc
+ln -fs ${BASEDIR}/profile ~/.profile
 # git
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -fs ${BASEDIR}/gitconfig ~/.gitconfig
+
+
+#Download some things
+wget https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
+sudo dpkg -i google-chrome-unstable_current_amd64.deb
+curl https://sdk.cloud.google.com | bash
