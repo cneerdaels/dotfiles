f4892bd1d8fed616e7114dc57abccc6ce6102e04
Mike Brown <mikecb@users.noreply.github.com>
2015-05-12 20:27:50 -0400

    Add installation of latest golang.

--- install.sh
+++ install.sh
@@ -16,3 +16,14 @@ wget https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
 sudo dpkg -i google-chrome-unstable_current_amd64.deb
 sudo apt-get -f install
 curl https://sdk.cloud.google.com | bash
+
+#install golang
+VERSION="1.4.2"
+DFILE="go$VERSION.linux-amd64.tar.gz"
+wget https://storage.googleapis.com/golang/$DFILE -O ~/Downloads/
+if [ $? -ne 0 ]; then
+    echo "Download failed! Exiting."
+    exit 1
+fi
+sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+mkdir -p "$HOME/go/{src,pkg,bin}"
