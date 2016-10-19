1b58aba7e08f17cea7c0822e95dc463817bb5b3a
Mike Brown <mikecb@users.noreply.github.com>
2016-10-19 16:09:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ else
 fi
 
 #install golang
-GOVERSION="1.7"
+GOVERSION="1.7.3"
 DFILE="go$GOVERSION.linux-amd64.tar.gz"
 wget https://storage.googleapis.com/golang/$DFILE -P ~/Downloads
 if [ $? -ne 0 ]; then
